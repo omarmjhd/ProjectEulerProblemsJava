8b3beaf3f5bfb2e470db3c5a93c642dd5ea79333
omujahid3 <omujahid3@gatech.edu>
2014-11-26 00:11:14 -0500

    Made more efficient, less checks are needed for same solution

--- src/Problem5.java
+++ src/Problem5.java
@@ -7,7 +7,6 @@
  */
 public class Problem5 {
 
-
     public static int smallest() {
 
         int small = 1;
@@ -15,10 +14,7 @@ public class Problem5 {
 
         while (!found) {
 
-
-            if ((small % 1 == 0) && (small % 2 == 0) && (small % 3 == 0) && (small % 4 == 0) && (small % 5 == 0) &&
-                    (small % 6 == 0) && (small % 7 == 0) && (small % 8 == 0) && (small % 9 == 0) && (small % 10 == 0) &&
-                    (small % 11 == 0) && (small % 12 == 0) && (small % 13 == 0) && (small % 14 == 0) && (small % 15 == 0) &&
+            if ((small % 11 == 0) && (small % 12 == 0) && (small % 13 == 0) && (small % 14 == 0) && (small % 15 == 0) &&
                     (small % 16 == 0) && (small % 17 == 0) && (small % 18 == 0) && (small % 19 == 0) && (small % 20 == 0)) {
 
                 return small;
@@ -40,7 +36,6 @@ public class Problem5 {
         int largest = p3.smallest();
 
         System.out.println("The solution is: " + largest);
-
 
     }
 }
